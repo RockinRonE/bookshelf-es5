158e010f03a82bd859f00db4e1ee34bf6db58258
Ronny Ewanek <ronnye@criticalmass.com>
2018-01-22 13:06:04 -0600

    Add form validation and alerts

--- app.js
+++ app.js
@@ -23,7 +23,21 @@ UI.prototype.clearFields = function() {
 	document.getElementById('title').value = ''
 	document.getElementById('author').value = ''
 	document.getElementById('isbn').value = ''
+}
 
+UI.prototype.showAlert = function(message, className) {
+	const div = document.createElement('div')
+	div.className = `alert ${className}`
+	div.appendChild(document.createTextNode(message))
+
+	const container = document.querySelector('.container')
+	const form = document.querySelector('#book-form')
+	// insert alert
+	container.insertBefore(div, form)
+
+	setTimeout(function() {
+		document.querySelector('.alert').remove();
+	}, 3000)
 
 }
 
@@ -37,8 +51,13 @@ document.getElementById('book-form').addEventListener('submit', e => {
 
 	const ui = new UI()
 
-	ui.addBookToList(book)
-	ui.clearFields()
+	if(title === '' || author === '' || isbn === '') {
+		ui.showAlert('Please fill in all fields', 'error')
+	} else {
+		ui.addBookToList(book)
+		ui.showAlert('Book Added!', 'success')
+		ui.clearFields()
+	}
 
-	console.log(ui)
+	
 })
