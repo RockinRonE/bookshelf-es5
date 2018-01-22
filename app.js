973acba069e293a29f78899b1a0b3f8dac5f2bdd
Ronny Ewanek <ronnye@criticalmass.com>
2018-01-22 12:27:39 -0600

    Add initial html and css setup

---
+++ app.js
@@ -0,0 +1,12 @@
+function Book (title, author, isbn) {
+	this.title = title
+	this.author = author
+	this.isbn = isbn
+}
+
+function UI() {}
+
+document.getElementById('book-form').addEventListener('submit', e => {
+	e.preventDefault()
+	console.log('test')
+})
